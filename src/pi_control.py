a1125f82e9f5e2f6404d6974e4b598fbe8477dc2
GreenEllefant <germanjellsworth@gmail.com>
2023-03-13 23:10:20 -0700

    Update pi_control.py

--- src/pi_control.py
+++ src/pi_control.py
@@ -13,58 +13,83 @@ class PI_Control:
     """! 
     This class implements a closed loop position control for a motor
     """
-    def __init__(self, gain, ki, kd, setpoint, encoder, motor):
+    def __init__(self, Kp, Ki, Kd, freq, setpoint, encoder, motor):
         """! 
         Creates a closed loop by initializing values
         used for closed loop control.
-        @param gain: sets the gain from the controller
+        @param Kp: gain for position
+        @param Ki: gain for integral
+        @param Kd: gain for derivative
+        @param freq: frequency of control system call
         @param setpoint: sets the initial setpoint for the controller
         @param encoder: takes an encoder_reader class for the system
         @param motor: takes a motor_driver class for the system
         """
-        self.gain = gain
-        self.ki = ki
-        self.kd = kd
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.freq = freq
+        self.pwm = 0
+        self.pwm_P = 0
+        self.pwm_I = 0
+        self.pwm_D = 0
         self.total_error = 0
         self.setpoint = setpoint
-        self.values = [0, 0]
+        #self.values = [0, 0]
         self.encoder = encoder
         self.motor = motor
         self.time = utime.ticks_ms()
         self.prev_time = utime.ticks_ms()
-        self.total_error = 0
 
     def run(self, setpoint):
         """! 
         Updates the system parameters
-        @param gain: sets the gain from the controller
-        @param setpoint: sets the initial setpoint for the controller
-        @param encoder: takes an encoder_reader class for the system
-        @param motor: takes a motor_driver class for the system
+        @param setpoint: sets the setpoint for the controller
         """
         self.setpoint = setpoint
         #self.values[0] = utime.ticks_ms() - self.time
         #self.values[1] = self.encoder.read()
+        
         if(utime.ticks_ms() - self.prev_time == 0):
-            print("oiuoiu")
             time = .001
         else:
             time = utime.ticks_ms() - self.prev_time
+            
+        sat = 80  # Saturation limit      
         self.total_error += (self.setpoint - self.encoder.read())
-        print(self.setpoint - self.encoder.read())
-        pwm = self.gain * (self.setpoint - self.encoder.read()) + self.kd * self.total_error / time + self.ki * self.total_error * time
-        if pwm > 100:
-            pwm = 99.9
-        if pwm < -100:
-            pwm = -99.9
-        self.motor.set_duty_cycle(pwm)
+        
+        self.pwm_P = self.Kp * (self.setpoint - self.encoder.read())
+        if self.pwm_P > sat:
+            self.pwm_P = sat
+        elif self.pwm_P < -sat:
+            self.pwm_P = -sat
+        
+        self.pwm_I = self.Ki * self.total_error / self.freq
+        if self.pwm_I > sat:
+            self.pwm_P = sat
+        elif self.pwm_I < -sat:
+            self.pwm_I = -sat
+        
+        # pwm_D = Kd * error / time
+        self.pwm_D = self.Kd * (self.setpoint - self.encoder.read()) * self.freq
+        if self.pwm_D > sat:
+            self.pwm_D = sat
+        elif self.pwm_D < -sat:
+            self.pwm_D = -sat
+        
+        self.pwm = self.pwm_P + self.pwm_I + self.pwm_D
+        if self.pwm > sat:
+            self.pwm = sat
+        elif self.pwm < -sat:
+            self.pwm = -sat
+        self.motor.set_duty_cycle(self.pwm)
         self.prev_time = utime.ticks_ms()
 
     def reset_values(self):
         """! 
         Resets the system values and time
         """
-        self.values = [0, 0]
+        #self.values = [0, 0]
         self.time = utime.ticks_ms()
 
     def set_setpoint(self, setpoint):
@@ -74,15 +99,21 @@ class PI_Control:
         """
         self.setpoint = setpoint
     
-    def set_Kp(self, gain):
+    def set_Kp(self, Kp):
         """!
         Sets a new controller gain
-        @param gain: The new controller gain
+        @param Kp: The new controller gain
         """
-        self.gain = gain
+        self.Kp = Kp
 
-    def set_Ki(self, gain):
-        self.ki = gain
+    def set_Ki(self, Ki):
+        self.Ki = Ki
+        
+    def set_Kd(self, Kd):
+        self.Kd = Kd
+        
+    def set_freq(self, freq):
+        self.freq = freq
 
     def print_values(self):
         """!
@@ -91,7 +122,21 @@ class PI_Control:
         for i in range(0, len(self.values[0])):
             print(str(self.values[0][i]) + "," + str(self.values[1][i]))
         
+    def get_pwm(self):
+        print(self.pwm)
+        
+    def get_pwm_P(self):
+        print(self.pwm_P)
+        
+    def get_pwm_I(self):
+        print(self.pwm_I)
+        
+    def get_pwm_D(self):
+        print(self.pwm_D)
+        
+# Run this test code when the file is run
 if __name__ == "__main__":
+    # Set up encoder pins
     en1_pin = pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.IN)
     en2_pin = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.IN)
     timer3 = pyb.Timer(8, prescaler=0, period=0xFFFF)
@@ -105,30 +150,24 @@ if __name__ == "__main__":
     m = Motor_Driver(en_pin, in1pin, in2pin, timer5)
     
     # Set up control class
-    Kp = 0.05       # Motor control parameter
-    Ki = 0.00005
-    kd = 0
-    c = PI_Control(Kp, Ki, kd, 0, e, m)
+    Kp = 0.000001       # Motor control parameter
+    Ki = 0.02
+    Kd = 0.00003
+    freq = 1/.002
+    c = PI_Control(Kp, Ki, Kd, freq, 0, e, m)
     
-    switch = False
     time = utime.ticks_ms()
     itime = time
     # Get references to the share and queue which have been passed to this task
-    while utime.ticks_ms()- itime < 6000:
+    print("// Run Forward")
+    while utime.ticks_ms()- itime < 3000:
         utime.sleep_ms(2)
-        c.run(-225*2)
-        #if(switch):
-        #    c.run(225 * 2)
-        #else:
-        #    c.run(-225 * 2)
-        #if(utime.ticks_ms() - time > 2000):
-        #    time = utime.ticks_ms()
-        #    if(switch):
-        #        switch = False
-        #    else:
-        #        switch = True
-    while utime.ticks_ms() - itime < 7000:
-        utime.sleep_ms(2)
-        c.run(0)
-    print("done")
+        c.run(-21657)
+        c.get_pwm()
+    print("// Return to start")
+#     while utime.ticks_ms() - itime < 6000:
+#         utime.sleep_ms(2)
+#         c.run(0)
+#         c.get_pwm()
     m.set_duty_cycle(0)
+    print("// TEST COMPLETE //")
