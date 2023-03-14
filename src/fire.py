29fbee33722028d7c0a27f58b4bbc7b1d8b7e52c
c4324r <baconlord123456@gmail.com>
2023-03-14 03:07:26 -0700

    werewR

--- src/fire.py
+++ src/fire.py
@@ -31,18 +31,21 @@ class fire:
 if __name__ == "__main__":
     m1_pin = pyb.Pin(pyb.Pin.board.PC2, pyb.Pin.OUT_PP)
     m2_pin = pyb.Pin(pyb.Pin.board.PC3, pyb.Pin.OUT_PP)
+    #zero_pin = pyb.Pin(pyb.Pin.board.PC4, pyb.Pin.IN)
     fire = fire(m1_pin, m2_pin)
     time = utime.ticks_ms()
-    #fire.flywheel(True)
-    #while(utime.ticks_ms() - time) < 3000:
-    #    utime.sleep_ms(1)
-    #time = utime.ticks_ms()
+    fire.flywheel(True)
+    while(utime.ticks_ms() - time) < 3000:
+        utime.sleep_ms(1)
+    time = utime.ticks_ms()
     fire.piston()
-    while(utime.ticks_ms() - time) < 5000:
+    while(utime.ticks_ms() - time) < 660 * 8:
+        #if(zero_pin.value() != 0):
+        #    print(zero_pin.value())
         utime.sleep_ms(1)
     fire.release()
     fire.flywheel(False)
-   
+    #zero_pin = pyb.Pin(pyb.Pin.board.PC4, pyb.Pin.IN)
         
 
 
